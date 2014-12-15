871f16e49ab0741148b68a82b26c0e3d8dda9688
Felix Chandrakumar <i@fc.id.au>
2014-12-16 00:11:09 +1100

    Initial Commit

---
+++ DNACalcFrm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace DNA_Calculator
+{
+    partial class DNACalcFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DNACalcFrm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.neanPercent = new System.Windows.Forms.Label();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(358, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BROWSE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.Location = new System.Drawing.Point(447, 255);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(44, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "y-str.org";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select File 2";
+            // 
+            // neanPercent
+            // 
+            this.neanPercent.Font = new System.Drawing.Font("OCR A Extended", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.neanPercent.ForeColor = System.Drawing.Color.Gray;
+            this.neanPercent.Location = new System.Drawing.Point(102, 72);
+            this.neanPercent.Name = "neanPercent";
+            this.neanPercent.Size = new System.Drawing.Size(306, 114);
+            this.neanPercent.TabIndex = 3;
+            this.neanPercent.Text = "0.00%";
+            this.neanPercent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Location = new System.Drawing.Point(12, 255);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(0, 13);
+            this.statusLbl.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(224, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "shared";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select File 1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "BROWSE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DNACalcFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 277);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusLbl);
+            this.Controls.Add(this.neanPercent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DNACalcFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DNA Calculator";
+            this.Load += new System.EventHandler(this.DNACalcFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label neanPercent;
+        private System.Windows.Forms.Label statusLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DNACalcFrm.cs
@@ -0,0 +1,273 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DNA_Calculator
+{
+    public partial class DNACalcFrm : Form
+    {
+
+        Dictionary<string, string[]> rsid = new Dictionary<string, string[]>();
+        long total_bp = 0;
+        string filename1 = null;
+        string filename2 = null;
+        long total_base_pairs = 0;
+        public DNACalcFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void DNACalcFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public byte[] Zip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    //msi.CopyTo(gs);
+                    CopyTo(msi, gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public byte[] Unzip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                {
+                    //gs.CopyTo(mso);
+                    CopyTo(gs, mso);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public void CopyTo(Stream src, Stream dest)
+        {
+            byte[] bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                filename2 = dialog.FileName;
+                total_bp = 0;
+                neanPercent.Text = "0.00%";
+                neanPercent.ForeColor = Color.Gray;
+                statusLbl.Text = "Calculating ...";
+                button1.Enabled = false;
+                backgroundWorker2.RunWorkerAsync();
+            }
+        }
+
+        private string getAutosomalText(string file)
+        {
+            string text = null;
+
+            if (file.EndsWith(".gz"))
+            {
+                StringReader reader = new StringReader(Encoding.UTF8.GetString(Unzip(File.ReadAllBytes(file))));
+                text = reader.ReadToEnd();
+                reader.Close();
+
+            }
+            else if (file.EndsWith(".zip"))
+            {
+                using (var fs = new MemoryStream(File.ReadAllBytes(file)))
+                using (var zf = new ZipFile(fs))
+                {
+                    var ze = zf[0];
+                    if (ze == null)
+                    {
+                        throw new ArgumentException("file not found in Zip");
+                    }
+                    using (var s = zf.GetInputStream(ze))
+                    {
+                        using (StreamReader sr = new StreamReader(s))
+                        {
+                            text = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            else
+                text = File.ReadAllText(file);
+            return text;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.y-str.org/");
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            StringReader reader = new StringReader(getAutosomalText(filename1));
+            string line = null;
+            string[] data = null;
+            long pos_start = 0;
+            long pos_end = 0;
+            string chr = null;
+            string pchr = null;
+            int i_chr = -1;
+            total_base_pairs = 0;
+            while((line=reader.ReadLine())!=null)
+            {
+                if (line.StartsWith("#") || line.StartsWith("RSID") || line.Trim().StartsWith("rsid"))
+                    continue;
+                line = line.Replace("\"", "").Replace("\t", ",");
+                data = line.Split(new char[] { ',' });
+                if (data.Length == 5)
+                    data[3] = data[3] + data[4];
+
+                chr = data[1];
+
+                if (!int.TryParse(chr, out i_chr))
+                    continue;
+                if (i_chr > 22 || i_chr <= 0)
+                    continue;
+
+                if (!rsid.ContainsKey(data[0]))
+                    rsid.Add(data[0], data);
+
+                if(chr!=pchr)
+                {
+                    total_base_pairs = total_base_pairs + (pos_end - pos_start);
+                    pos_start = long.Parse(data[2]);
+                }
+                pos_end = long.Parse(data[2]);
+                pchr = chr;
+            }
+            reader.Close();
+            //
+            if(pchr==chr)
+                total_base_pairs = total_base_pairs + (pos_end - pos_start);            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            statusLbl.Text = "Please select second file ...";
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            StringReader reader = new StringReader(getAutosomalText(filename2));
+            string line = null;
+            string[] data = null;            
+            long segment_start = 0;
+            long segment_end = 0;
+            string chr=null;
+            string pchr=null;
+            int i_chr=-1;
+            int snp_count = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.StartsWith("#") || line.StartsWith("RSID"))
+                    continue;
+                line = line.Replace("\"", "").Replace("\t", ",");
+                data = line.Split(new char[] { ',' });
+                if (data.Length == 5)
+                    data[3] = data[3] + data[4];
+                if(rsid.ContainsKey(data[0]))
+                {
+                    chr=data[1];
+
+                    if (!int.TryParse(chr, out i_chr))
+                        continue;
+                    if (i_chr > 22 || i_chr <= 0)
+                        continue;
+                    else if (!isMatch(rsid[data[0]][3], data[3]) || chr != pchr)
+                    {
+                        if (segment_end - segment_start >= 100000 && snp_count>30) // 100000 bp
+                        {
+                            if (rsid[data[0]][3] == data[3] || rsid[data[0]][3] == ReverseString(data[3]))
+                                total_bp = total_bp + (segment_end - segment_start)*2;
+                            else
+                                total_bp = total_bp + (segment_end - segment_start);
+                        }
+                        segment_start = long.Parse(rsid[data[0]][2]);
+                        snp_count = 0;
+                    }
+                    segment_end = long.Parse(rsid[data[0]][2]);
+                    pchr=chr;
+                    snp_count++;
+                }
+            }
+            reader.Close();
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        private bool isMatch(string p1, string p2)
+        {
+            foreach (char c1 in p1.ToCharArray())
+                foreach (char c2 in p2.ToCharArray())
+                    if (c1 == c2)
+                        return true;
+            return false;
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // because total_bp is HIR, we need to calcualate percentage for each allele.
+            double percent = total_bp * 100.0 / (total_base_pairs * 2);// excluding X chromosome and positions not tested by DNA companies and mtdna otherwise it is 3.2 billion bp
+            if (percent > 100)
+                percent = 100;
+            neanPercent.Text = percent.ToString("#0.00") + "%";
+            neanPercent.ForeColor = Color.Black;
+            statusLbl.Text = "";
+            button1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                filename1 = dialog.FileName;
+                total_bp = 0;
+                rsid.Clear();
+                neanPercent.Text = "0.00%";
+                neanPercent.ForeColor = Color.Gray;
+                statusLbl.Text = "Loading ...";
+                button1.Enabled = false;
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+    }
+}
